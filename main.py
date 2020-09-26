9a2bb04ccd92f7395487196fd40e038449686e5d
eatmelon <357191233@qq.com>
2020-09-26 16:22:04 +0800

    txt

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ def show():
 def write_txt(data, txt_dir):
     with open(txt_dir, "w+") as f:  # 追加写
         f.write("id        sv        B\n")
-        for i in range(params.provider_num):
+        for i in range(10):
             f.write(str(data[i].item()) + " ")
         f.write("\n")
 
